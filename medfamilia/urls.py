9e0b43800f1cc71dad675811661e302c574596d0
JacksonAlves123 <jacksonalves541@gmail.com>
2020-04-30 10:24:55 -0300

    adicionando secao de agendar consulta na home

--- medfamilia/urls.py
+++ medfamilia/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('teste', views.teste, name='teste'),
     path('especialidades/', views.especialidades, name='especialidades'),
     path('especialidades/id/', views.especialidade_especifica, name='especialidade_especifica'),
 ]
