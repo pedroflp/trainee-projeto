1388505b74c3fc4435eed234c8a93a618902e31f
JacksonAlves123 <jacksonalves541@gmail.com>
2020-05-02 10:05:52 -0300

    adicionando form e pre selecionando a especialidade na pagina de especifica

--- medfamilia/views.py
+++ medfamilia/views.py
@@ -58,9 +58,53 @@ def especialidades (request):
     return render(request, 'especialidades.html', {'especialidades': especialidades})
 
 
-def especialidade_especifica (request, id):
-    especialidade = get_object_or_404(Especialidade, pk=id)
-    return render(request, 'especialidade_especifica.html', {'especialidade': especialidade})
+class especialidade_especifica (View):
+    def get (self, request, id):
+        especialidade = get_object_or_404(Especialidade, pk=id)
+
+        #initial define valores pré definidos para os campos que podem ser
+        #alterados
+        form = ConsultaForm(initial = {'especialidade': id})
+
+        return render(request, 'especialidade_especifica.html', 
+                        {'especialidade': especialidade, 'form': form})
+
+
+    def post(self, request, id):
+        especialidade = get_object_or_404(Especialidade, pk=id)
+        form = ConsultaForm(request.POST)
+        fragmento = "#form"
+
+        if form.is_valid():
+            try:
+                datetime.strptime(form.data['data'], "%d/%m/%Y")
+
+            except:
+                return render(request, 'especialidade_especifica.html', {'especialidade': especialidade,
+                                              'form': form,
+                                              'erro': 'Formato de data inválido. ex: 01/01/2020',
+                                              'fragmento': fragmento})
+                
+
+            expressaoSemEspaco = re.compile(r'\(\d{2}\)\d{4,5}-\d{4}\Z')
+            expressaoComEspaco = re.compile(r'\(\d{2}\) \d{4,5}-\d{4}\Z')
+
+            if expressaoSemEspaco.match(form.data['telefone']) or expressaoComEspaco.match(form.data['telefone']):
+                consulta = form.save(commit=False)
+                consulta.respondida = False
+                consulta.save()
+
+                return redirect('confirmacao_consulta', nome=consulta.nome)
+
+            else:
+                return render(request, 'especialidade_especifica.html', {'especialidade': especialidade,
+                                              'form': form,
+                                              'erro': 'Formato de telefone inválido. ex: (01) 98765-4321 ou (01) 8765-4321',
+                                              'fragmento':fragmento})
+
+        return render(request, 'especialidade_especifica.html', {'especialidade': especialidade,
+                                              'form': form,
+                                              'fragmento':fragmento})
 
 
 def confirmacao_consulta (request, nome):
