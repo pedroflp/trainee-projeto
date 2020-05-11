f46abc0af481cc1dcb8e34532e681a7373d1e424
JacksonAlves123 <jacksonalves541@gmail.com>
2020-05-11 16:11:32 -0300

    funcionalidades de marcar como respondida e em espera no django admin

--- medfamilia/admin.py
+++ medfamilia/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
 from .models import Especialidade, Consulta, QuemSomos
 
+
+def consulta_respondida(modeladmin, request, queryset):
+    queryset.update(respondida=True)
+consulta_respondida.short_description = "Marcar como respondida"
+
+def consulta_em_espera(modeladmin, request, queryset):
+    queryset.update(respondida=False)
+consulta_em_espera.short_description = "Marcar como em espera"
+
+class ConsultaAdmin(admin.ModelAdmin):
+    actions = [consulta_respondida, consulta_em_espera]
+
 # Register your models here.
 
 admin.site.register(Especialidade)
-admin.site.register(Consulta)
+admin.site.register(Consulta, ConsultaAdmin)
 admin.site.register(QuemSomos)
